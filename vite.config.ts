400672acaf8689ec855e2a219f12c5113c20b989
pro_rokky <vlad6005@gmail.com>
2026-05-07 23:14:27 +0300

    feat: add module federation

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,36 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import federation from '@originjs/vite-plugin-federation'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    federation({
+      name: "host-app",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Navbar": "./src/components/layouts/Navbar.vue",
+      },
+      remotes: {
+        dashboard: "http://localhost:8001/remoteEntry.js",
+      },
+      shared: ["vue", "vue-router", "pinia"],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  build: {
+    target: "esnext",
+    minify: true,
+    cssCodeSplit: true,
+  },
+  server: {
+    port: 8000,
   },
 })
